8ff688965fe9e2b82868cfdbe7ff99d0f86fa990
Brian Choi <brian@Brian-PC.hypervisor.ssnull.pw>
2021-08-23 01:05:51 +0800

    Updated build

---
+++ precache-manifest.5d97b7aa50616d3159fdf3d3f672230a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf0f7fa931582d7f1b8e7ab7bde9b2e",
+    "url": "/horizoverlay/index.html"
+  },
+  {
+    "revision": "f58c2caa98b4f00d7d8a",
+    "url": "/horizoverlay/static/css/main.05e1a232.chunk.css"
+  },
+  {
+    "revision": "83419b46128ddf343906",
+    "url": "/horizoverlay/static/js/2.5419f153.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/horizoverlay/static/js/2.5419f153.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f58c2caa98b4f00d7d8a",
+    "url": "/horizoverlay/static/js/main.18657ea8.chunk.js"
+  },
+  {
+    "revision": "30f65308d7bdcba22167",
+    "url": "/horizoverlay/static/js/runtime-main.9a241b58.js"
+  },
+  {
+    "revision": "e1e188201661cad8e440f57279b85a23",
+    "url": "/horizoverlay/static/media/mog.e1e18820.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/horizoverlay/precache-manifest.e69bd2d2f8b3e552ccdd89f677fe3e6e.js"
+  "/horizoverlay/precache-manifest.5d97b7aa50616d3159fdf3d3f672230a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/horizoverlay/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
